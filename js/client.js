02d4d01892893bf1583f22572efeb90499aa6421
xlatyn <xlatyn@mendelu.cz>
2025-05-29 21:12:23 +0100

    Approval section ui fixes

--- js/client.js
+++ js/client.js
@@ -33,8 +33,8 @@ TrelloPowerUp.initialize({
         return t.popup({
           title: 'Manage Approvals',
           url: './manage-approvals.html',
-          height: 800,
-          width: 450
+          height: 600,
+          width: 400
         });
       }
     }];
