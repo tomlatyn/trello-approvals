b3a926bc7fc4b5e6af6c57e58028cf39b88c793b
xlatyn <xlatyn@mendelu.cz>
2025-10-27 15:02:02 +0100

    Capitals

--- js/features/card-detail-badges.js
+++ js/features/card-detail-badges.js
@@ -1,24 +1,15 @@
 // Card detail badges feature handler
 function cardDetailBadgesHandler(t) {
-  return t.get('card', 'shared', 'approvals', null).then(function(approvalData) {
-    // Determine badge text based on whether approvals exist
-    var badgeText = 'Set approvals';
-
-    if (approvalData && approvalData.members && Object.keys(approvalData.members).length > 0) {
-      badgeText = 'Change approvals';
+  return [{
+    title: 'Approvals',
+    text: 'Manage Approvals',
+    color: 'purple',
+    callback: function(t) {
+      return t.popup({
+        title: 'Manage Approvals',
+        url: './views/manage-approvals.html',
+        height: 700
+      });
     }
-
-    return [{
-      title: 'Approvals',
-      text: badgeText,
-      color: 'purple',
-      callback: function(t) {
-        return t.popup({
-          title: 'Manage Approvals',
-          url: './views/manage-approvals.html',
-          height: 700
-        });
-      }
-    }];
-  });
+  }];
 }
