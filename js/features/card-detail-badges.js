644a8ac0171f240365ba03a8df9e3f5c1734acca
xlatyn <xlatyn@mendelu.cz>
2025-10-27 14:37:31 +0100

    Added card detail badges

---
+++ js/features/card-detail-badges.js
@@ -0,0 +1,24 @@
+// Card detail badges feature handler
+function cardDetailBadgesHandler(t) {
+  return t.get('card', 'shared', 'approvals', null).then(function(approvalData) {
+    // Determine badge text based on whether approvals exist
+    var badgeText = 'Set approvals';
+
+    if (approvalData && approvalData.members && Object.keys(approvalData.members).length > 0) {
+      badgeText = 'Change approvals';
+    }
+
+    return [{
+      title: 'Approvals',
+      text: badgeText,
+      color: 'purple',
+      callback: function(t) {
+        return t.popup({
+          title: 'Manage Approvals',
+          url: './manage-approvals.html',
+          height: 700
+        });
+      }
+    }];
+  });
+}
